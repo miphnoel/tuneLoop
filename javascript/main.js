5bd0b9d3ed9d0e28fe56aa0900800551baba282c
Michael Noel <miphnoel@gmail.com>
2017-07-05 18:39:18 -0400

    Add loop bar

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.getElementById('grid');
-  for (let c = 0; c < 8; c++) {
+  for (let c = 0; c < 16; c++) {
     let column = document.createElement('ul');
     column.className = `col-${c}`;
 
-    for (let r = 0; r < 8; r++) {
+    for (let r = 0; r < 16; r++) {
       let space = document.createElement('li');
-      space.className = `row-${r}`;
+      let classes = [`row-${r}`, `hue-${r % 7}`, 'unselected'];
+      classes.forEach(className => space.classList.add(className));
       space.onclick = (e) => toggleSelected(e);
       column.appendChild(space);
     }
 
     grid.appendChild(column);
   }
+
+  const loopBar = document.createElement('div');
+  loopBar.className = "loop-bar";
+  loopBar.style.left = 0;
+  grid.appendChild(loopBar);
+
+  const loopId = setInterval(function() {
+    const startLeft = parseFloat(loopBar.style.left);
+    const newLeft = (startLeft + .25) % 100;
+    loopBar.style.left = `+${newLeft}%`;
+  }, 12);
 });
 
 const toggleSelected = (e) => {
-  e.currentTarget.classList.toggle('selected');
+  e.currentTarget.classList.toggle('unselected');
 };
